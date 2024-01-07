a579068d589e29d584188303b4048afe504cb620
stoneshik <strebilly@mail.ru>
2024-01-07 14:02:12 +0300

    Сделал основную страницу и набросал структуру

---
+++ src/components/headers/MainHeader.jsx
@@ -0,0 +1,18 @@
+import { NavLink } from "react-router-dom";
+
+
+export const MainHeader = () => {
+    return (
+        <div id="header" className="container">
+            <div className="container"><NavLink to="index" className="none-underline"><h2>TypoFast</h2></NavLink></div>
+            <div className="container">
+                <ul className="menu row">
+                    <li><NavLink to="about">о нас</NavLink></li>
+                    <li><NavLink to="help">помощь</NavLink></li>
+                    <li><NavLink to="map_without_login">мы на карте</NavLink></li>
+                    <li><NavLink to="login">вход</NavLink></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
