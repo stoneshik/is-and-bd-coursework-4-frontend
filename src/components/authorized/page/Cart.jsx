c04f7abdce40e9f2b892295edee621035dbb8131
stoneshik <strebilly@mail.ru>
2024-01-12 00:13:34 +0300

    рабочее удаление

--- src/components/authorized/page/Cart.jsx
+++ src/components/authorized/page/Cart.jsx
@@ -1,13 +1,18 @@
+import superagent from "superagent";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+
 import { Header } from "../header/Header";
 import { Footer } from "../footer/Footer";
-import {useEffect, useState} from "react";
-import superagent from "superagent";
-import {Link} from "react-router-dom";
 
 
 export function Cart() {
     const [errorMessage, setErrorMessage] = useState('');
     const [orders, setOrders] = useState([]);
+    const [checkedOrders] = useState({});
+    const [amount, setAmount] = useState(0.0);
+    const [updateVar, setUpdate] = useState(0);
     useEffect(() => {
         superagent
             .get('/api/order/get_not_paid')
@@ -32,6 +37,32 @@ export function Cart() {
                 }
             )
     }, []);
+    const updateChecked = (orderNum, orderAmount) => {
+        checkedOrders[orderNum] = !checkedOrders[orderNum];
+        if (checkedOrders[orderNum]) {
+            setAmount(amount + orderAmount);
+            return;
+        }
+        setAmount(amount - orderAmount);
+    };
+    const removeRow = (orderNum, orderAmount) => {
+        const result = window.confirm("Вы уверены что хотите удалить заказ?");
+        if (!result) {
+            return;
+        }
+        for (let i = 0; i < orders.length; i++) {
+            const order = orders[i];
+            if (order['orderNum'] === orderNum) {
+                if (checkedOrders[orderNum]) {
+                    setAmount(amount - orderAmount);
+                }
+                orders.splice(i, 1);
+                delete checkedOrders[orderNum];
+                setUpdate(updateVar + 1);
+                return;
+            }
+        }
+    };
     const createRowTable = (order) => {
         const orderNum = order['orderNum'];
         const orderType = order['orderType'];
@@ -46,9 +77,13 @@ export function Cart() {
             return;
         }
         const orderTypeString = (orderType === 'PRINT')? 'печать': 'сканирование';
+        if (checkedOrders[orderNum] === undefined) {
+            checkedOrders[orderNum] = false;
+        }
         return (
             <tr>
-                <td><input type="checkbox" autoComplete="off" onChange="updateAmount(this);"/></td>
+                <td><input type="checkbox" autoComplete="off"
+                           onChange={() => {updateChecked(orderNum, orderAmount)}}/></td>
                 <td className="num">
                     <Link to={"/order_print/" + orderNum} title="Подробнее о заказе">{orderNum}</Link>
                 </td>
@@ -57,7 +92,8 @@ export function Cart() {
                 <td>{orderAmount} руб.</td>
                 <td className="address">{orderAddress}</td>
                 <td>
-                    <img src={"./img/cross.png"} alt="cross" style={{width: "24px"}} onClick="remove(this);"/>
+                    <img src={"./img/cross.png"} alt="cross" style={{width: "24px"}}
+                         onClick={() => removeRow(orderNum, orderAmount)}/>
                 </td>
             </tr>
         );
@@ -88,7 +124,7 @@ export function Cart() {
                         </table>
                     </div>
                     <div id="total" className="container">
-                        <h3>Итого: <span id="amount">0</span> руб.</h3>
+                        <h3>Итого: <span id="amount">{amount}</span> руб.</h3>
                         <div className="button" onClick="handlingPay();">Оплатить</div>
                     </div>
                 </div>
